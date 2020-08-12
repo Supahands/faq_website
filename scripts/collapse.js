99b8b029695c3fde74a95cf160e91b83c980e86d
heretodesign <senwelo.works@gmail.com>
2020-08-12 15:44:03 +0800

    fix accordion to close if another is open

---
+++ scripts/collapse.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    $('.collapse').on('show.bs.collapse', function () {
+    
+    // hide all accordion except the clicked one
+    $('.collapse').not(this).collapse('hide');
+  });
+});
